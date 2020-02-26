e27510394518ed6f6e2f461c7f66cd4c9f8b8656
ZaStack <stackzpersonal@gmail.com>
2020-02-25 17:10:56 -0800

    Working axios

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ import MissionList from './components/MissionsList';
 
 export default function App() {
     const [data, setData] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const getMissions = () => {
-        axios.get('https://api.spacexdata.com/v3/missions')
+        setLoading(true);
+        // setError(false);
+        axios
+            .get('https://api.spacexdata.com/v3/missions')
             .then(res => {
                 setData(res.data);
-                console.log('Res', res);
+                console.log('Res', res.data);
+                setLoading(false);
             })
-            .then(console.log(data))
-            .catch(err => console.log('Data error', err));
+            .catch(error => {
+                setError(error)
+            })
     };
 
     return (
         <div className='App'>
             <MissionForm onClick={getMissions} />
+            { error && <div style={{color: `red`}}> An error occurred while fetching missions</div> }
+            { loading && <div style={{color: `green`}}>Fetching missions.</div>}
             {data.map(mission => (
                 <MissionList
                     missionName={mission.mission_name}
--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const MissionList = (props) => {
+    console.log('Props log', props)
     return (
         <div>
-            <h1>{props.mission_name}</h1>
+            <h1>{props.missionName}</h1>
             <p>Manufacturer: {props.manufacturers}</p>
+            {/* <p>{props.payload_ids}</p>
+            <p>Website: {props.website}</p>
+            <p>Wiki: {props.wikipedia}</p> */}
         </div>
     )};
 
